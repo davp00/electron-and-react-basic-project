28884328c1c06d8038e3d97373a09366f37b1abe
davp <davp@localhost.localdomain>
2019-03-26 12:38:08 -0500

    configured

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hola mundo te dice DAVP
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Aprende electron y react
           </a>
         </header>
       </div>
